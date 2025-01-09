61d34662f21701b12afd57c88bae5e7f82cd6840
stagib <147700418+stagib@users.noreply.github.com>
2025-01-09 16:40:04 +1100

    fix: validate comment exist before creating reaction

--- app/routers/comment.py
+++ app/routers/comment.py
@@ -96,9 +96,13 @@ def react_to_comment(
     user: dict = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    db_post = db.query(Post).filter(Post.id == post_id).first()
-    if not db_post:
-        raise HTTPException(status_code=404, detail="Post not found")
+    db_comment = (
+        db.query(Comment)
+        .filter(Comment.post_id == post_id, Comment.id == comment_id)
+        .first()
+    )
+    if not db_comment:
+        raise HTTPException(status_code=404, detail="Comment not found")
 
     db_reaction = (
         db.query(CommentReaction)
