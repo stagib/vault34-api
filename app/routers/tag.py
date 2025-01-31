6790d7fcc4d1d1d799cc05bf748fdb8cba1a8c64
stagib <147700418+stagib@users.noreply.github.com>
2025-01-31 14:52:53 +1100

    feat: add type filter in tag search

--- app/routers/tag.py
+++ app/routers/tag.py
@@ -4,9 +4,9 @@ from fastapi_pagination.ext.sqlalchemy import paginate
 from sqlalchemy.orm import Session
 
 from app.database import get_db
+from app.enums import TagType
 from app.models import Tag
 from app.schemas import TagBase
-
 
 router = APIRouter(tags=["Tag"])
 
@@ -14,11 +14,15 @@ router = APIRouter(tags=["Tag"])
 @router.get("/tags", response_model=Page[TagBase])
 def get_tags(
     query: str = Query(None, min_length=1),
+    type: TagType = Query(None),
     db: Session = Depends(get_db),
 ):
-    if not query:
-        tags = db.query(Tag)
-        return paginate(tags)
+    tags = db.query(Tag)
 
-    tags = db.query(Tag).filter(Tag.name.ilike(f"%{query}%"))
+    if query:
+        tags = tags.filter(Tag.name.ilike(f"%{query}%"))
+
+    if type:
+        tags = tags.filter(Tag.type == type)
+
     return paginate(tags)
